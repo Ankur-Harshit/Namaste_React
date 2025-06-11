c1371e189f96e6922dc651cb0a19f2649c1a7a68
Ankur-Harshit <ankurharshit1@gmail.com>
2025-06-11 14:21:05 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,28 @@
+// const heading = React.createElement("h1", {id : "heading", xyz:"abc"}, "Hello From React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// making nested stuffs //
+/*
+<div parent>
+    <div children>
+        hello i am h1
+        hello i am h2
+    <div>
+<div>
+*/
+
+// React.createElement returns an object, while rendering it is converted to DOM that is understood by Browser
+
+const parent = React.createElement("div", {id:"parent"}, 
+    React.createElement("div", {id:"children"}, 
+        [React.createElement("h1",{},"Hello i am h1"),
+        React.createElement("h1",{},"Hello i am h2")
+        ]
+))
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
