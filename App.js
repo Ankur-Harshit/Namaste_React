6413745c26fb1fbce3816ce83c022b1d4fd8dde3
Ankur-Harshit <ankurharshit1@gmail.com>
2025-06-21 14:17:10 +0530

    episode-03-React elements and components

--- App.js
+++ App.js
@@ -1,30 +1,29 @@
-// const heading = React.createElement("h1", {id : "heading", xyz:"abc"}, "Hello From React");
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-// making nested stuffs //
-/*
-<div parent>
-    <div children>
-        hello i am h1
-        hello i am h2
-    <div>
-<div>
-*/
-
-// React.createElement returns an object, while rendering it is converted to DOM that is understood by Browser
-
 import React from "react";
 import ReactDOM from 'react-dom/client';
-const parent = React.createElement("div", {id:"parent"}, 
-    React.createElement("div", {id:"children"}, 
-        [React.createElement("h1",{},"Hello i am h1"),
-        React.createElement("h1",{},"Hello i am h2")
-        ]
-))
 
-console.log(parent);
+// using pure React
+// const heading = React.createElement("h1", {id:"heading"}, "Namaste React🚀");
+// console.log(heading);
+
+// using JSX
+
+// React element
+
+const Title = ()=> (
+<h1 className="head">
+    Namaste React Using JSX🚀</h1>
+)
+
+// inside curly we can write js
+// React component
+const Heading = ()=>(
+    <div id="container">
+        <Title />
+        <Title></Title>
+        {Title()}
+        <h1 className="heading">React Functional Component</h1>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Heading />);
