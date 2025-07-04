4a4d8c3ba4f1fa65c9aef06d893d92eadb5135c1
Ankur-Harshit <ankurharshit1@gmail.com>
2025-07-04 11:56:38 +0530

    episode-06-ShimmerUI-More Hooks-Why React is Fast

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,58 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockdata";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import Shimmer from "./shimmer";
 let cnt = 0;
 const Body = () => {
     // React Hooks
     // useState
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
-    return (
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    const [filteredListOfRestaurants, SetFilteredListOfRestaurants] = useState([]);
+    const [searchText, setSearchText] = useState([]);
+    // useEffect
+
+    useEffect(()=>{
+        fetchData();
+    }, [])
+
+    const fetchData = async ()=>{
+        const data = await fetch("https://foodfire.onrender.com/api/restaurants?lat=23.3556&lng=85.3324&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        SetFilteredListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+
+    // Shimmer UI 
+
+    return listOfRestaurants.length ===0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+                <input
+                    type="text" className="search-box" value={searchText}
+                    onChange={(e)=>{
+                        setSearchText(e.target.value);
+                    }}
+                />
+                <button onClick={()=>{
+                    console.log(searchText);
+                    const filteredList = listOfRestaurants.filter((res)=>
+                        res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                    )
+                    SetFilteredListOfRestaurants(filteredList);
+                }}>
+                    Search
+                </button>
                 <button className="filter-btn" 
                 onClick = {()=>{
-                    cnt++;
                     const filteredList = listOfRestaurants.filter((res)=>
                         res.info.avgRating >= 4.5
                     );
-                    setListOfRestaurants(filteredList);
-                    console.log(filteredList);
-                    console.log("Clicked "+cnt+" Times");
+                    SetFilteredListOfRestaurants(filteredList);
                 }}>
                     Top Rated Restaurants
                 </button>
             </div>
             <div className="res-container">
-                {listOfRestaurants.map((restaurant) => (
+                {filteredListOfRestaurants.map((restaurant) => (
                     <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                 )
             )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 const Header = () => {
+    const [btnName, setbtnName] =useState("login");
     return(
             <div className="header">
                 <div className="logo-container">
@@ -12,6 +14,11 @@ const Header = () => {
                         <li>About Us</li>
                         <li>Contact Us</li>
                         <li>Cart</li>
+                        <button className="login" onClick={()=>{
+                            btnName==="login"?setbtnName("logout"):setbtnName("login");
+                        }}>
+                            {btnName}
+                        </button>
                     </ul>
                 </div>
             </div>
